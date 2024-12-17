fdc5cd6340cf44fc51bf1b07f88d505ff9be49f6
mitraghav <amitraghav8111@gmail.com>
2024-12-17 20:06:11 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "info",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Krishna",
 	description: "Admin and Bot info.",
 	commandCategory: "...",
 	cooldowns: 1,
@@ -24,36 +24,36 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =                                     
-["https://imgur.com/a/jQ5RpTH"];
-var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
-(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
+["https://i.imgur.com/t4eL74O.jpeg" , "https://i.imgur.com/6B4nkuT.jpeg"];
+var callback = () => api.sendMessage({body:`🌹𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍FO 🌹
 
-☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
+𝗕𝗢𝗧 𝗡𝗔𝗠𝗘÷
+♪♪♪♪♪♪♪『 ☞︎︎︎ ${global.config.BOTNAME} ☜︎︎︎ 』.♪♪♪♪♪♪♪
 
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ 𝗦𝗛𝗔𝗔𝗕 𝗝𝗜💔🥀
-
-🙈bot andmin owner facebook id link🙈➪ www.facebook.com/profile.php?id=61565406584020.rajput.official 💞🕊️
-
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @𝗗𝗛𝗔𝗥𝗜𝗬𝗔 𝗦𝗛𝗔𝗔𝗕😇
+╭───────────╮
+🌻𝐎𝐖𝐍𝐄𝐑 ꧁☞︎🍒💖SHAAB💖🍒☜︎꧂
+╰───────────╯ ╭────────────╮
+🥀𝐁𝐎𝐓 𝐀𝐃𝐌𝐈𝐍 ꧁☞︎💖👑 DHARIYA SHAAB👑💖☜︎꧂ 🤸  💞 
+╰────────────╯
+🍇𝐎𝐖𝐍𝐄𝐑 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊🍇
+───────https://www.facebook.com/profile.php?id=61565406584020&mibextid=ZbWKwL
 
 ✧══════•❁❀❁•══════✧
 
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+💖𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫💖☞︎︎︎ [ ${global.config.PREFIX} ] ☜︎︎︎
 
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ 𝗦𝗛𝗔𝗔𝗕 𝗝𝗜
+🕊️ 𝗨𝗣𝗧𝗜𝗠𝗘 🕊️
 
-🥳UPTIME🥳
+🕊️𝗧𝗼𝗱𝗮𝘆 𝗜𝘀🕊️ ☞︎︎︎ [ ${juswa} ] ☜︎︎︎
 
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+⚡𝗕𝗼𝘁 𝗜𝘀 𝗥𝘂𝗻𝗶𝗻𝗴⚡ ☞︎︎︎ [ ${hours}:${minutes}:${seconds}. ] ☜︎︎︎
 
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
-
-✅Thanks for using ${global.config.BOTNAME} Bot🖤
+💖💖💖💖💖𝗧𝗵𝗻𝗸 𝘆𝗼𝘂 𝘀𝗼 𝗺𝘂𝗰𝗵 𝗴𝘂𝘆𝘀💖💖💖💖
 
 
 🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
 ┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
-🌸✦✧✧✧✧✰🍒𝗦𝗛𝗔𝗔𝗕 𝗝𝗜🌿✰✧✧✧✧✦🌸
+🌸✦✧✧✧✧✰🍒SHAAB JI🌿✰✧✧✧✧✦🌸
 ┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
 
 
